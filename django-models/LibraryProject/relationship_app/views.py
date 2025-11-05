abf37bb673f07624a59783b140e5d90e77dee8ad
SaadJabrane20 <sjabrane20@gmail.com>
2025-11-05 14:28:42 +0100

    templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,24 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Author, Book, Library, Librarian
+from django.shortcuts import render
+from models import *
+from .models import Library
+from django.views.generic.detail import DetailView
+# Create your views here.
+def list_books(request):
+    book = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html')
 
-# List all books by a specific author
-def list_books(request, author_name=None):
-    if author_name:
-        author = get_object_or_404(Author, name=author_name)
-        books = Book.objects.filter(author=author)
-    else:
-        books = Book.objects.all()
-        author = None
-    return render(request, 'list_books.html', {'books': books, 'author': author})
-
-
-# Show details for a specific library
-def library_detail(request, library_name):
-    library = get_object_or_404(Library, name=library_name)
-    books = library.books.all()
-    librarian = getattr(library, 'librarian', None)
-    return render(
-        request,
-        'library_detail.html',
-        {'library': library, 'books': books, 'librarian': librarian},
-    )
+def library_detail(request):
+    library = Library.objects.all()
+    return render(request, 'relationship_app/library_detail.html')
