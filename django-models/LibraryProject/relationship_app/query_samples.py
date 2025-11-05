24911820f0a55b9e09af06392a013e7b05ebf23e
SaadJabrane20 <sjabrane20@gmail.com>
2025-11-05 13:50:25 +0100

    relationship app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,5 +4,5 @@ books = Author.objects.get(name=author_name)
 books = Book.objects.filter(author=author)
 books = books.all()
 library = Library.objects.get(name=library_name)
-librarian = Librarian.objects.get(name="")
+librarian = Librarian.objects.get(library="")
 library = librarian.library
