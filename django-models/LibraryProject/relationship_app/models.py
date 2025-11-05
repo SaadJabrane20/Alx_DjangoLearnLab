10da7491e6d7601e48afc2e072e2ab43d5128fc8
SaadJabrane20 <sjabrane20@gmail.com>
2025-11-05 14:27:38 +0100

    templates

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 class Library(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
     def __str__(self):
         return self.name
