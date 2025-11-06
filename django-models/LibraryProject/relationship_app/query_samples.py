b2aa2a1ab28c5e46e7878482a362d72f2722f2ae
SaadJabrane20 <sjabrane20@gmail.com>
2025-11-06 14:34:29 +0100

    restart task

--- django-models/LibraryProject/relationship_app/query_samples.py
+++
@@ -1,8 +0,0 @@
-from models import *
-
-books = Author.objects.get(name=author_name)
-books = Book.objects.filter(author=author)
-books = books.all()
-library = Library.objects.get(name=library_name)
-librarian = Librarian.objects.get(library="")
-library = librarian.library
--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-from .views import list_books, LibraryDetailView
-urlpatterns = [
-    path('list_books/', views.list_books, name='list_books'),
-    path('library_detail/<int:pk>/', views.library_detail, name='library_detail'),
-]
