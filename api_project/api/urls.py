d5f304b2c6312029105c2a52bf57d358944a9fdd
SaadJabrane20 <sjabrane20@gmail.com>
2025-11-18 09:26:56 +0100

    building first Restfulapi with router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,13 +1,12 @@
 from rest_framework import routers
-from .views import BookList
-from django.urls import path
+from .views import BookList, BookViewSet
+from django.urls import path, include
 
+router = routers.DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
-# router = routers.DefaultRouter()
-# router.register(r'books', BookList)
-
-# urlpatterns = router.urls
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls)),  # This includes all routes registered with the router
 ]
