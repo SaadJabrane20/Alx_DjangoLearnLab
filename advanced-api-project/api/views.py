9e40fda9c87bb7eae8e51e00ee128673dc5bc3af
SaadJabrane20 <sjabrane20@gmail.com>
2025-11-25 08:34:02 +0100

    Implementing Filtering, Searching, and Ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # Create your views here.
 
