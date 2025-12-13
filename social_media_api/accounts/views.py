84f72cd9fa6fe53512818bccd3e50992b213de32
SaadJabrane20 <sjabrane20@gmail.com>
2025-12-13 19:48:24 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 from .models import CustomUser
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class RegisterAPIView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -34,3 +36,23 @@ class ProfileAPIView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+class FollowUserAPIView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        target_user = get_object_or_404(self.get_queryset(), id=user_id)
+
+        if target_user == request.user:
+            return Response(
+                {"error": "You cannot follow yourself"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        target_user.followers.add(request.user)
+
+        return Response(
+            {"message": f"You are now following {target_user.username}"},
+            status=status.HTTP_200_OK
+        )
