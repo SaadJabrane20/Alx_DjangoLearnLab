595eb4a7a6020582e37101833fa3b5851ee0e0ce
SaadJabrane20 <sjabrane20@gmail.com>
2025-12-13 19:55:40 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('profile/', ProfileAPIView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUserAPIView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserAPIView.as_view(), name='unfollow-user'),
 ]
