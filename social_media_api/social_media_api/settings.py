6faa96ed4ed70922e291f142c540a11b939476d9
SaadJabrane20 <sjabrane20@gmail.com>
2025-12-13 16:11:14 +0100

    Implementing Posts and Comments Functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -135,4 +136,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
